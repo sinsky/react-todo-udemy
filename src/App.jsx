0257c41117ccee0ee6a3a3f26e8f55d804f75846
sin-sky-sora <60662802+sin-sky-sora@users.noreply.github.com>
2021-10-06 13:20:44 +0900

    ガワをreactで作成

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
+
 import "./styles.css";
 export const App = () => {
+  const [noCompleteTodos, setNoCompleteTodos] = useState(["Doit"]);
+  const [completeTodos, setCompleteTodos] = useState(["complete"]);
   return (
     <>
       <section class="input-area">
@@ -12,11 +15,30 @@ export const App = () => {
       </section>
       <section class="nocomplete-area">
         <p class="title">未達成Todo</p>
-        <ul id="nocomp-items"></ul>
+        <ul id="nocomp-items">
+          {noCompleteTodos.map((todoItem) => {
+            return (
+              <li class="list-row" key={todoItem}>
+                <span class="todo-title">{todoItem}</span>
+                <button class="moveComplateTodo">完了</button>
+                <button class="deleteTodo">削除</button>
+              </li>
+            );
+          })}
+        </ul>
       </section>
       <section class="complete-area">
         <p class="title">完了したTodo</p>
-        <ul id="comp-items"></ul>
+        <ul id="comp-items">
+          {completeTodos.map((todoItem) => {
+            return (
+              <li class="list-row" key={todoItem}>
+                <span class="todo-title">{todoItem}</span>
+                <button class="moveNoCompleteTodo">戻す</button>
+              </li>
+            );
+          })}
+        </ul>
       </section>
 
       <script src="src/index.js"></script>
