918357887b0247a7e8d40082476860b622eaa988
sin-sky-sora <60662802+sin-sky-sora@users.noreply.github.com>
2021-10-06 11:51:08 +0900

    create default html

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
 import React from "react";
 import "./styles.css";
 export const App = () => {
-  return <div></div>;
+  return (
+    <>
+      <section class="input-area">
+        <input type="text" placeholder="prease todo write!" id="todoText" />
+        <button id="todoAdded">add</button>
+      </section>
+      <section class="nocomplete-area">
+        <p>未達成Todo</p>
+        <ul id="nocomp-items"></ul>
+      </section>
+      <section class="complete-area">
+        <p>完了したTodo</p>
+        <ul id="comp-items"></ul>
+      </section>
+
+      <script src="src/index.js"></script>
+    </>
+  );
 };
